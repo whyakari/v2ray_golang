72c431703af09e1156bcf2cd184cf021b12669d7
Akari Chan <akariondev@gmail.com>
2023-06-08 04:11:47 +0000

    modified: pkg/v2ray/submenu_v2ray.go Updated V2Ray submenu

--- pkg/v2ray/submenu_v2ray.go
+++ pkg/v2ray/submenu_v2ray.go
@@ -1,1 +1,25 @@
 package v2ray
+
+import "fmt"
+
+func SubMenuV2ray() string {
+	fmt.Println("V2RAY Core Magisk")
+	fmt.Println("")
+	fmt.Println("Necessario ter instalado o modulo do magisk:")
+	fmt.Println("V2RAY For Android")
+	fmt.Println("")
+	fmt.Println("1 - Iniciar serviços V2RAY")
+	fmt.Println("2 - Parar serviços V2RAY")
+	fmt.Println("3 - Mudar configuração V2RAY")
+	fmt.Println("4 - Instalar sudo")
+	fmt.Println("5 - Corrigir instalação do Modulo")
+	fmt.Println("6 - Roteamento")
+	fmt.Println("7 - Visualizar arquivo")
+	fmt.Println("0 - Voltar")
+	fmt.Println("")
+
+	var opts string
+	fmt.Print("Escolha sua opção: ")
+	fmt.Scanln(&opts)
+	return opts
+}
