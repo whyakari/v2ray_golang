dcf5d4fc71bdba13331cf2263ce5b0e75f312912
Akari Chan <akariondev@gmail.com>
2023-06-26 20:42:49 +0000

    added new function ascii text

--- pkg/ascii/ascii.go
+++ pkg/ascii/ascii.go
@@ -1,1 +1,24 @@
 package ascii
+
+import (
+	"fmt"
+	"os"
+	"github.com/whyakari/v2ray_golang/pkg/clean"
+)
+
+func Ascii_Text(opt string) string {
+    clean.Clear()
+
+    if "sair" == opt || "3" == opt {
+        fmt.Println(`
+ ___
+  | |_   _. ._  |    \_/ _
+  | | | (_| | | |<    | (_) |_|
+        `)
+    }
+    
+    os.Exit(0)
+
+    return opt
+
+}
